f7b98a0e0760dd78d4cdb4e9b3bf5c2dc1846e18
cecibet <ceci.bet21@gmail.com>
2021-12-13 18:28:48 -0300

    Add Display Errors

--- js/script.js
+++ js/script.js
@@ -4,6 +4,21 @@ var divCityInput = document.querySelector(".div-input");
 var inputCity = document.getElementById("add-city-value");
 const button = document.getElementById("btn");
 var buttonToInput = document.getElementById("btn-add");
+var showError = document.getElementById("show-error");
+var errorDiv = document.getElementById("div-error");
+
+var selectArr;
+
+window.onload = function () {
+  selectArr = JSON.parse(localStorage.getItem("selectArr"));
+  if (selectArr != null) {
+    for (i = 0; i < selectArr.length; ++i) {
+      selectCity.options.add(new Option(selectArr[i], selectArr[i]));
+    }
+  } else {
+    selectArr = [];
+  }
+};
 
 inputCity.addEventListener("keyup", function (event) {
   event.preventDefault();
@@ -14,7 +29,7 @@ inputCity.addEventListener("keyup", function (event) {
 buttonToInput.disabled = false;
 function validateData() {
   if (!selectCity.value) {
-    alert("invalid");
+    alert("No ha seleccionado una ciudad válida");
     return false;
   }
   return true;
@@ -28,43 +43,90 @@ function displayCityInput() {
 
 function validateCity() {
   if (!inputCity.value) {
-    alert("complete el campo");
+    alert("Ingrese el nombre de la ciudad");
     return false;
   }
   return true;
 }
 
-function addCity() {
+function cityExists() {
   if (validateCity()) {
-    var newCity = inputCity.value;
-    selectCity.options.add(new Option(newCity, newCity));
-    alert("La ciudad se agregó con éxito");
-    inputCity.value = "";
-    divCityInput.style.display = "none";
-    buttonToInput.disabled = false;
+    for (i = 0; i < selectCity.length; ++i) {
+      if (selectCity.options[i].value == inputCity.value) {
+        alert("La ciudad ya se encuentra en la lista");
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+function addCity() {
+  removeError();
+  if (cityExists()) {
+    addCityOption();
+    saveData();
+    clearInput();
     $("#cities-select option:last").attr("selected", "selected");
-    } else {
-    alert("La ciudad ya está en lista");
+  } else {
     inputCity.value = "";
   }
 }
 
+function addCityOption() {
+  var newCity = inputCity.value;
+  selectCity.options.add(new Option(newCity, newCity));
+  alert("La ciudad se agregó con éxito");
+}
+
+function deleteCity() {
+  selectCity.remove(selectCity.selectedIndex);
+  saveData();
+  alert("La ciudad fue eliminada.");
+  result.style.display = "none";
+}
+
+function saveData() {
+  selectArr = [];
+  for (var i = 0; i < selectCity.options.length; i++) {
+    if (selectCity[i].value != "") {
+      selectArr.push(selectCity[i].text);
+    }
+  }
+  localStorage.setItem("selectArr", JSON.stringify(selectArr));
+}
+function clearInput() {
+  inputCity.value = "";
+  divCityInput.style.display = "none";
+  buttonToInput.disabled = false;
+  showError.innerText = "";
+  errorDiv.style.display = "none";
+}
+
 async function checkWeather() {
+  removeError();
+  clearInput();
   if (validateData()) {
-    var city = selectCity.value;
-    const response = await fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&appid=3936d0749fdc3124c6566ed26cf11978&units=metric&lang=es"
-    );
-    const data = await response.json();
-    console.log(data);
-    displayResult(data);
+    try {
+      var city = selectCity.value;
+      const response = await fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&appid=3936d0749fdc3124c6566ed26cf11978&units=metric&lang=es"
+      );
+      const data = await response.json();
+      console.log(data);
+      displayResult(data);
+    } catch (error) {
+      console.log(error);
+      displayError();
+    }
   }
 }
 
 function displayResult(data) {
   const { name } = data;
+  const { country } = data.sys;
   const { description, icon } = data.weather[0];
   const { temp, humidity, feels_like, pressure, temp_max, temp_min } =
     data.main;
@@ -74,7 +136,7 @@ function displayResult(data) {
   document.querySelector(".icon").src =
     "http://openweathermap.org/img/wn/" + icon + "@4x.png";
   document.querySelector(".tiempo").innerText = "TIEMPO EN";
-  document.querySelector(".city").innerText = name;
+  document.querySelector(".city").innerText = name + ", " + country;
   document.getElementById("date").innerHTML = getDate();
   document.querySelector(".description").innerText = description;
   document.querySelector(".temp").innerText = temp.toFixed(1) + "°C";
@@ -93,3 +155,19 @@ function getDate() {
   var time = today.getHours() + ":" + today.getMinutes();
   return "Hasta las " + time + ", ART";
 }
+
+function displayError() {
+  errorDiv.style.display = "block";
+  showError.innerText =
+    "La ciudad ingresada no se encuentra en la base de datos.";
+}
+
+function removeError() {
+  showError.innerText = "";
+  errorDiv.style.display = "none";
+}
+
+setTimeout(function () {
+  showError.innerText = "";
+  errorDiv.style.display = "none";
+}, 7000);
